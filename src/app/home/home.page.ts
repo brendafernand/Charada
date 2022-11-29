eb9278c70aa311478858fb3959d5c0478d05cd71
BrendaFernand <brendafernandas324@gmail.com>
2022-11-28 21:58:03 -0300

    Projeto finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  charada: string = '';
+  resposta: string = '';
+  respostaSecreta: string = '';
+  animacao: boolean = false;
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  ngOnInit(){
+    this.solicitarCharada();
+   
+  }
+
+  solicitarCharada(){
+    this.animacao = false;
+    const url = "http://lucasreno.kinghost.net/charada";
+    this.http.get<any[]>(url).subscribe( resposta => {
+      this.charada = resposta[0].pergunta;
+      this.respostaSecreta = resposta [0].resposta;
+      this.resposta='';
+      this.animacao = true;
+    });
+  }
+
+  solicitarResposta(){
+    this.resposta = this.respostaSecreta;
+  }
 
 }
